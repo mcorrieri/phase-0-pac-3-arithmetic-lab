267e3602158ee0245c653ecae0a6604f496a6a24
Michael <Mlcorrieri@gmail.com>
2021-03-04 12:29:18 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+function subtract(num1, num2) {
+  let result = num1 - num2;
+  return result;
+}
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+function divide(num1, num2) {
+  let result = num1 / num2;
+  return result;
+}
+
+function increment(a) {
+  let result = a + 1;
+  return result;
+}
+
+function decrement(a) {
+  let result = a - 1;
+  return result;
+}
+
+function makeInt(a) {
+  let result = parseInt(a, 10);
+  return result;
+}
+
+function preserveDecimal(a) {
+  let result = parseFloat(a);
+  return result;
+}
